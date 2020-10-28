06b34d7c8b21593f0fd4939fd7dc6dbe44b1178a
Tom Hamer <thomasjhamer@gmail.com>
2020-10-28 11:53:27 +0000

    created a 16x16 window with cells and other ui

---
+++ general.js
@@ -0,0 +1,44 @@
+
+const canvasSize = 8;
+
+
+const canvas = document.querySelector("#canvas");
+
+
+
+
+
+
+for (var i=0;i<canvasSize;i++){
+
+	const row = document.createElement("div");
+
+	row.classList.add("row");
+	canvas.appendChild(row);
+
+
+	for(var j=0;j<canvasSize;j++){
+		const cell = document.createElement("div");
+		const idNum = i*canvasSize+j;
+		cell.classList.add("cell");
+		cell.id = "id" + idNum.toString();
+		row.appendChild(cell);
+
+	}
+
+}
+
+window.addEventListener("mouseover", function(e){
+	if(e.target.className != "cell") return;
+	console.log(e);
+
+	const cell = e.target.id;
+})
+
+
+
+
+
+
+
+
